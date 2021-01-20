6632a82773854533d50d982ec595d3375b1a5e3e
BenWolfaardt <35372441+BenWolfaardt@users.noreply.github.com>
2021-01-20 21:55:40 +0100

    C/C++ .vscode setup for Building & Debugging

---
+++ C_C++/hello_world.cpp
@@ -0,0 +1,16 @@
+// From https://code.visualstudio.com/docs/cpp/config-mingw
+
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main() {
+    vector<string> msg {"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"};
+
+    for (const string& word : msg) {
+        cout << word << " ";
+    }
+    cout << endl;
+}
